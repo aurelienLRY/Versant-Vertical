eadaaed2d7e59673c5113bd1694bd0bf111c0a01
aurelienLRY <leroyaurelien11@gmail.com>
2024-04-05 16:17:44 +0200

    01- React-router-dom  / vitest / sass

---
+++ client/src/assets/test/check.test.js
@@ -0,0 +1,13 @@
+import { describe, it , expect } from "vitest";
+
+describe('Verification de l\'environement de test ', () => {
+    
+it('Doit retourner true', () => {
+expect(true).toBe(true);
+  });
+  
+it('Doit retourner false', () => {
+    expect(false).toBe(false);
+    });
+});
+
