f6f9fd94f19d06687694e0c49402e61471845470
Leroy aurelien <83220559+aurelienLRY@users.noreply.github.com>
2024-04-09 16:41:12 +0200

    Modal de connexion (#9)

--- client/src/components/footer/index.jsx
+++ client/src/components/footer/index.jsx
@@ -5,6 +5,9 @@ import { useDispatch } from "react-redux";
 import { logout } from "../../redux/reducers/authSlice";
 import Login from "../login"; // Import the Login component
 import "./footer.scss";
+import { FaFacebookSquare, FaInstagram } from "react-icons/fa";
+import moduleStyle from "../../assets/sass/main.module.scss";
+import { Tooltip } from "antd";
 
 function Footer() {
   const dispatch = useDispatch();
@@ -22,12 +25,27 @@ function Footer() {
   return (
     <footer className="footer" data-testid="footer">
       <Modal isOpened={modalIsOpened} Closed={handleModal}>
-       <Login isConnect={(e) => setModalIsOpened(e.target)}/>
+        <Login isConnect={(e) => setModalIsOpened(e.target)} />
       </Modal>
-      <div>1</div>
-      <div>2 </div>
       <div>
-        <button onClick={handleModal}>
+        <Tooltip
+          title="Facebook"
+          placement="top"
+          color={moduleStyle.toolTipBackground}
+        >
+          <FaFacebookSquare />
+        </Tooltip>
+        <Tooltip
+          title="Instagram"
+          placement="top"
+          color={moduleStyle.toolTipBackground}
+        >
+          <FaInstagram />
+        </Tooltip>
+      </div>
+      <div>occitanie-evasion</div>
+      <div>
+        <button onClick={handleModal} className="btn-secondary-outline small">
           {isAuthenticated ? "Déconnexion" : "Connection"}{" "}
         </button>
       </div>
--- client/src/components/modal/index.jsx
+++ client/src/components/modal/index.jsx
@@ -1,19 +1,36 @@
 import React from "react";
+import moduleStyle from "../../assets/sass/main.module.scss";
+import { Tooltip } from "antd";
 import "./modal.scss";
 
-function Modal({ isOpened, children , Closed }) {
+/* iconographie */
+import { IoIosCloseCircle } from "react-icons/io";
 
-const setClosed = (e) => {
-Closed(e);
-}
+function Modal({ isOpened, children, Closed }) {
+  const setClosed = (e) => {
+    Closed(e);
+  };
 
   if (isOpened) {
     return (
       <div className="modal" data-testid="modal">
-       
-        <div className="modal_content"> 
-        <button onClick={(e) => setClosed(e)}>Fermer </button>
-        {children}</div>
+        <div className="modal_content">
+          <div className="modal_header">
+          <Tooltip
+                title="Fermer"
+                placement="top"
+                color={moduleStyle.toolTipBackground}
+              >
+            <IoIosCloseCircle
+              onClick={(e) => setClosed(e)}
+              className="modal_close"
+            />
+            </Tooltip>
+          </div>
+          <div className="modal_body">
+            {children} 
+          </div>   
+        </div>
       </div>
     );
   }
